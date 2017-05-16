71d63845f672f224c35d675699027dda1cb27a37
高伟冬 <280823086@qq.com>
2017-05-16 17:12:59 +0800

    Update

--- sql/day1.sql
+++ sql/day1.sql
@@ -9,7 +9,7 @@ USE db_1702;  -- 使用
 SHOW TABLES;  --  显示表格
 
 CREATE TABLE t_student (  -- 创建表格
-  sno    VARBINARY(10),
+  sno    VARBINARY(10),-- 可变长字符类型
   sname  VARBINARY(10),
   gender CHAR(1),
   age    INT(2)
@@ -20,3 +20,22 @@ FROM t_student;
 
 INSERT INTO t_student  -- 插入数据库
 VALUE ('2017001', '张三', '男', 18); -- 值
+/*
+SELECT - extracts data from a database           选择
+          从数据库提取数据
+UPDATE - updates data in a database                 更新
+        更新数据库中的数据
+DELETE - deletes data from a database              删除
+        从数据库中删除数据
+INSERT INTO - inserts new data into a database    插入数据
+              将新数据插入数据库
+CREATE DATABASE - creates a new database        创建数据库
+                  创建一个新的数据库
+ALTER DATABASE - modifies a database            修改数据库
+DROP DATABASE - deletes a database              删除数据库
+CREATE TABLE - creates a new table                创建表
+ALTER TABLE - modifies a table                   修改表
+DROP TABLE - deletes a table                       删除表
+CREATE INDEX - creates an index (search key)     增加索引
+DROP INDEX - deletes an index                    删除索引
+*/
---
+++ src/main/java/mysql/JDBC.java
@@ -0,0 +1,13 @@
+package mysql;
+
+/**
+ * Created by 高伟冬 on 2017/5/16.
+ * MySQL
+ * 17:09
+ * 星期二
+ */
+public class JDBC {
+    public static void main(String[] args) {
+
+    }
+}
