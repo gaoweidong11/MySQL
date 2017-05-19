e56a8936c04052a54bd9974d63ba69fb913ab4c0
高伟冬 <280823086@qq.com>
2017-05-19 09:25:55 +0800

    Update

---
+++ sql/day5.sql
@@ -0,0 +1,20 @@
+SELECT *
+FROM db_1702.student;
+
+SELECT *
+FROM db_1702.student
+LIMIT 2;
+
+SELECT *
+FROM db_1702.student
+LIMIT 2 OFFSET 3;
+
+SELECT *
+FROM scott.emp;
+
+SELECT *
+FROM db_1702.student
+LIMIT 2,1;
+
+SELECT *
+FROM scott.salgrade;
